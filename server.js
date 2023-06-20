853d109999d38c869c949b972ead0aea15087196
Stina Aygol <StinaAygol@proton.me>
2023-06-20 15:58:14 -0700

    Set header properties of res object and sent a html response.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  // Look in chrome dev tools > network tab > header subtab to see if these properties have been set.
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>Hello Nodejs</h1>');
+  res.end();
+});
 
 const PORT = process.env.PORT || 5000;
 
