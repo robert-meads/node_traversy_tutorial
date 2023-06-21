141acb32afe6db85587e6543a55f65d3824440a6
Stina Aygol <StinaAygol@proton.me>
2023-06-20 17:12:40 -0700

    Simplified setting properties with only one method: writeHead(). Added request url to display different results.

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const http = require('http');
+const employees = require('./data/employees.json');
 
 const server = http.createServer((req, res) => {
-  // Look in chrome dev tools > network tab > header subtab to see if these properties have been set.
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Hello Nodejs</h1>');
-  res.end();
+  if (req.url === '/api/employees') {
+    // setHeader, setStatus can be condensed into one function: writeHead as this func takes status and content-type property.
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // We don't need to res.write and res.end. We can send data via res.end.
+    res.end(JSON.stringify(employees));
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify('URL NOT FOUND!'));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
