0f1100b4df9583ec878fe3d8c33b12e2632c3212
Stina Aygol <StinaAygol@proton.me>
2023-06-26 16:48:27 -0700

    Removed tutorial on printing current file/directory.

--- server.js
+++ server.js
@@ -23,7 +23,3 @@ const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// How to check where you are:
-// console.log(`Current working directory: ${process.cwd()}`);
-// console.log(`Directory name is ${__dirname}`);
-// console.log(`Filename is ${__filename}`);
