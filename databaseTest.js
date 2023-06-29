c31970f042de373c43c329f1d8c03b41a65f1677
Stina Aygol <StinaAygol@proton.me>
2023-06-29 12:25:08 -0700

    Debug: deleted comments about usage of promises vs callback for mongodb nodejs driver version 5.6.0.

--- databaseTest.js
+++ databaseTest.js
@@ -1,25 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
 
-// The latest mongodb nodejs driver:
-// use command: 'npm list monogodb'
-// I have mongodb@5.6.0 version.
-// This version uses promises and doesn't use callbacks it seems. Which is why the code below seems to not work.
-// I can be wrong.
-
-// async function startMongodb() {
-//   try {
-//     await MongoClient.connect('mongodb://localhost:27017', function (err, db) {
-//       if (err) {
-//         throw err;
-//       }
-
-//       console.log('hello from mongodb!');
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
 const uri = 'mongodb://localhost:27017';
 
 async function connect() {
