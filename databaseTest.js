bffa5deb93cba43baaafb9cd19c466a34876427c
Stina Aygol <StinaAygol@proton.me>
2023-06-30 12:44:14 -0700

    Tutorial: Find one document.

--- databaseTest.js
+++ databaseTest.js
@@ -2,6 +2,11 @@ const MongoClient = require('mongodb').MongoClient;
 
 const uri = 'mongodb://localhost:27017';
 
+// Be careful if collection is huge. This is a costly op.
+async function printAll(cursor) {
+  console.log(await cursor.toArray());
+}
+
 async function connect() {
   const client = new MongoClient(uri);
 
@@ -11,6 +16,11 @@ async function connect() {
 
     const students = db.collection('students');
     const results = students.find();
+    // await printAll(results);
+
+    // find Huey. .find() returns a cursor that you need to .toArray() to convert to something I can print in console. .findOne() returns something I don't need to call .toArray().
+    const huey = await students.findOne({ name: 'Huey' });
+    console.log('Here is huey ', huey);
   } catch (err) {
     console.log(err);
   } finally {
