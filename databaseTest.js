9ebd122482d591be4c62ed9bccb84e7f35b40ea9
Stina Aygol <StinaAygol@proton.me>
2023-06-30 12:27:22 -0700

    Deleted previous tutorial on database properties and .find().

--- databaseTest.js
+++ databaseTest.js
@@ -8,26 +8,9 @@ async function connect() {
   try {
     await client.connect();
     const db = client.db('school');
-    // console.log(`database is ${db.databaseName}`);
-    // const collections = await db.collections();
-    // collections.forEach((collection) => {
-    //   console.log(`Collection ${collection.collectionName}`);
-    // });
 
     const students = db.collection('students');
-    // .find() returns a cursor which is an iterable containing documents.
     const results = students.find();
-    // console.log('results: ', await results.toArray());
-    // console.table(await results.toArray());
-
-    // how to move to next document in collection via the cursor, results.
-    // let test = await results.hasNext();
-    // let i = 0;
-    // while (test) {
-    //   console.log(test, i++);
-    //   console.log(await results.next());
-    //   test = await results.hasNext();
-    // }
   } catch (err) {
     console.log(err);
   } finally {
